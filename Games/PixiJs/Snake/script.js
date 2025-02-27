7ec9db67e2ed432d6d8059c520d411bb41d0eb26
MohamadDalal <mohammad.moddy@gmail.com>
2025-02-28 00:37:38 +0100

    Trying to add touch control to the snake game

--- Games/PixiJs/Snake/script.js
+++ Games/PixiJs/Snake/script.js
@@ -420,6 +420,32 @@ class Grid {
         }
     }
 
+    // Source: https://stackoverflow.com/a/56663695
+    // Checking the direction of a swipe for touch controls
+    const touchStart = [0,0]
+    const touchEnd = [0,0]
+
+    document.addEventListener('touchstart', e => {
+        touchStart[0] = e.changedTouches[0].screenX
+        touchStart[1] = e.changedTouches[0].screenY
+      })
+      
+      document.addEventListener('touchend', e => {
+        touchEnd[0] = e.changedTouches[0].screenX
+        touchEnd[1] = e.changedTouches[0].screenY
+        //console.log(touchStart, touchEnd)
+        const angle = Math.atan2(touchEnd[1] - touchStart[1], touchEnd[0] - touchStart[0]);
+        if (angle > Math.PI/4 && angle < 3*Math.PI/4 && snake.velocity.y == 0){
+            el.dispatchEvent(new KeyboardEvent('keypress',{'key':'ArrowUp'}));
+        } else if (angle < -Math.PI/4 && angle > -3*Math.PI/4 && snake.velocity.y == 0){
+            el.dispatchEvent(new KeyboardEvent('keypress',{'key':'ArrowDown'}));
+        } else if (angle > 3*Math.PI/4 && angle < -3*Math.PI/4 && snake.velocity.x == 0){
+            el.dispatchEvent(new KeyboardEvent('keypress',{'key':'ArrowLeft'}));
+        } else if (angle < Math.PI/4 && angle > -Math.PI/4 && snake.velocity.x == 0){
+            el.dispatchEvent(new KeyboardEvent('keypress',{'key':'ArrowRight'}));
+        }
+      })
+
     // Testing the different behaviour between circle and rect
     // const testGraphic = new PIXI.Graphics();
     // testGraphic.circle(tileSize/2, tileSize/2, tileSize/2);
